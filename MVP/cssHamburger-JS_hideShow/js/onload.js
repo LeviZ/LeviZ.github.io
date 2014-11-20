1056458aab4f7dc403d49f75c837c1de74c7485f
Levi Zortman <levizortman@gmail.com>
2014-11-19 17:42:41 -0800

    began splitting up css into separate files(not finished), added some content

---
+++ MVP/cssHamburger-JS_hideShow/js/onload.js
@@ -0,0 +1,9 @@
+// Opening page state:
+
+window.onload = function() {
+  setTimeout(function(){
+    document.getElementById('nav-trigger').checked = true;
+  }, 2000);
+  closeLabel.style.display = 'none';
+  setDisplayNone();
+};
