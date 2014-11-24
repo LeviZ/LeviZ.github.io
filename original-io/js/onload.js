4cc8787b2b9ee6a9a3e7a77117e3d56bf1f6423e
Sheyna <sheyna@users.noreply.github.com>
2014-11-24 11:04:02 -0800

    removed old files from repo.

--- original-io/js/onload.js
+++
@@ -1,9 +0,0 @@
-// Opening page state:
-
-window.onload = function() {
-  setTimeout(function(){
-    document.getElementById('nav-trigger').checked = true;
-  }, 2000);
-  closeLabel.style.display = 'none';
-  setDisplayNone();
-};
